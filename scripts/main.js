f5939a9eebe8bc35a7bf99c403567c6e9bd88f77
Kozlov-ae <allek-007@list.ru>
2021-01-21 09:37:33 +0300

    feat: Добавил вывод даты, небольшой рефакторинг.

--- scripts/main.js
+++ scripts/main.js
@@ -1,66 +1,3 @@
-function Guid() {
-      return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-        var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
-        return v.toString(16); 
-      });
-    }
-
-const arr = [
-    {
-        "id": "1",
-        "userName": "Олег Васильевич",
-        "nickname": "vasil",
-        "text": "Где детонатор?",
-		"postDate": "02.14.2012, 05:00"
-    },
-    {
-        "id": "12",
-        "userName": "Brock",
-        "nickname": "brock",
-        "text": "По своей сути рыбатекст является альтернативой традиционному lorem ipsum, который вызывает у некторых людей недоумение при попытках прочитать рыбу текст. В отличии от lorem ipsum, текст рыба на русском языке наполнит любой макет непонятным смыслом и придаст неповторимый колорит советских времен.",
-        "postDate": "02.05.2012, 13:27",
-        "img": "https://fish-text.ru/images/logo.png",
-        "liked": "true",
-        "likes": 50
-    },
-    {
-        "id": "13",
-        "userName": "Raamin",
-        "nickname": "raamin",
-        "text": "По своей сути рыбатекст является альтернативой традиционному lorem ipsum, который вызывает у некторых людей недоумение при попытках прочитать рыбу текст. В отличии от lorem ipsum, текст рыба на русском языке наполнит любой макет непонятным смыслом и придаст неповторимый колорит советских времен.",
-        "postDate": "03.11.2012, 10:30",
-        "likes": 999
-    },
-    {
-        "id": "14",
-        "userName": "Дональд",
-        "nickname": "trampampam",
-        "text": "Зарегался на вк, хороший сервис и не банят",
-        "postDate": "02.05.2012, 13:27",
-        "img": "https://i2.wp.com/media.globalnews.ca/videostatic/news/vamt80qbaq-94ovmaxjqg/trumptwitterupdate.jpg?w=500&quality=70&strip=all",
-        "likes": 50
-        
-    }
-    ,
-    {
-        "id": "15",
-        "userName": "Олег Васильевич",
-        "nickname": "vasil",
-        "text": "Где детонатор?",
-        "postDate": "02.14.2012, 05:00",
-        "img": "https://www.meme-arsenal.com/memes/27606cb09d10f670389750cffb4d900d.jpg",
-        "likes": 666
-    },
-    {
-        "id": "123",
-        "userName": "Raamin",
-        "nickname": "raamin",
-        "text": "По своей сути рыбатекст является альтернативой традиционному lorem ipsum, который вызывает у некторых людей недоумение при попытках прочитать рыбу текст. В отличии от lorem ipsum, текст рыба на русском языке наполнит любой макет непонятным смыслом и придаст неповторимый колорит советских времен.",
-        "postDate": "03.11.2012, 10:30",
-        "likes": 999
-    }
-];
-
 class Twitter {
     constructor({listElem}) {
         this.tweets = new Posts(arr);
@@ -91,7 +28,7 @@ class Twitter {
 }
 
 class Posts {
-    constructor({posts = arr} = {}) {
+    constructor({posts = []} = {}) {
         this.posts = posts;
     }
     
@@ -111,14 +48,15 @@ class Posts {
     }
 }
 class Post {
-    constructor(params) {
-        this.id = params.id !== undefined ? params.id : Guid();
-        this.userName = params.userName;
-        this.nickname = params.nickName;
-        this.postDate = params.postDate;
-        this.text = params.text;
-        this.img = params.img;
-        this.likes = params.likes;
+    constructor({id, userName, nickname, postDate, text, img, likes = 0}) {
+        
+        this.id = id || generateGuid();
+        this.userName = userName;
+        this.nickname = nickName;
+        this.postDate = postDate ? new Date(postDate) : new Date();
+        this.text = text;
+        this.img = img;
+        this.likes = likes;
         this.liked = false;
     }
 
@@ -130,6 +68,24 @@ class Post {
         else {
             this.likes--;
         }
+    }
+
+    generateGuid() {
+        return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+          var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+          return v.toString(16); 
+        });
+    }
+
+    getDate(){
+        const options = {
+            year: 'numeric',
+            month: 'numeric',
+            day: 'numeric',
+            hour: '2-digit',
+            minutes: '2-digit',
+        }
+        return this.postDate.toLocaleString('ru-RU', options)
     }
 }
 
